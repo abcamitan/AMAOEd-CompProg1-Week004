ded38fb2b5c3bcd39e474089823ec18ff13ed620
Aries Camitan <ariescamitan@yahoo.com>
2018-08-18 15:50:21 +0800

    compiled version for Week010

--- src/Week010/task2/main.cpp
+++ src/Week010/task2/main.cpp
@@ -8,7 +8,7 @@ int main()
 {
   Person pip("Aries Camitan", "Guiguinto, Bulacan", "Male", 37, "Software Developer");
 
-  cout << "[Personal Details] " << pip.getInformation() << endl;
+  cout << "[Personal Details] \n" << pip.getInformation() << endl;
 
   cout << endl;
   return EXIT_SUCCESS;
